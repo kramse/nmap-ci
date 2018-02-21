22332189a5bc9c902673726a58151f6775872fed
Henrik Kramshoej <hlk@nordu.net>
2018-02-21 16:11:15 +0100

    Use a script which exist in the old Ubuntu Nmap version

--- ci/test.sh
+++ ci/test.sh
@@ -8,5 +8,6 @@ pushd nmap-ci
    echo Hello world
    apt-get update
    apt-get -y install nmap
-   nmap -A -p 22 --script ssh-auth-methods.nse 10.80.0.194
+   nmap -A -p 22 --script ssh2-enum-algos.nse 10.80.0.194
+#   nmap -A -p 22 --script ssh-auth-methods.nse 10.80.0.194
 popd
