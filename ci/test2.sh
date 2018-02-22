491ccad45919b196ccf211cfdf51d81d1da19c5b
Henrik Kramshoej <hlk@nordu.net>
2018-02-22 09:45:56 +0100

    Funky

--- ci/test2.sh
+++ ci/test2.sh
@@ -6,6 +6,6 @@ set -e -x
 
 pushd nmap-ci
    echo SSH algorithms
-   nmap -A -p 22 --script ssh2-enum-algos.nse 10.80.0.194
+   nmap -A -p 22 --script ssh-auth-methods.nse 10.80.0.194
 #   nmap -A -p 22 --script ssh-auth-methods.nse 10.80.0.194
 popd
